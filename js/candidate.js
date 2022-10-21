909aa9f303744ebb63c0e97034b466b9f419cd85
weilingshine <weiling.go@gmail.com>
2022-10-21 09:12:42 +0800

    20221021_測試機

---
+++ js/candidate.js
@@ -0,0 +1,5 @@
+candidate = '<div class="slogan">\
+        <div class="quite animate__animated animate__zoomInDown" data-aos="zoom-in" data-aos-delay="1000"><img src="images/Union.svg"></div>\
+        <div class="title" data-aos="fade-up"><img src="images/slogan.png"></div>\
+    </div>';
+document.write(candidate);
